06831f1d67079028b6cfc395b448f3c451099739
grantbradshaw <grant.bradshaw@outlook.com>
2018-10-14 17:09:05 -0400

    Initializing sass for project

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from sassutils.wsgi import SassMiddleware
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/sass', 'static/css', '/static/css')
+})
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 
